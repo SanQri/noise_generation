a45ba1657f1370d2c58d55d45d4eabae9baa31b9
Galiullin Nikolai <galiullinnikolai@gmail.com>
2016-05-19 07:15:22 +0200

    TheFirstCommit
    Check

---
+++ NoiseGeneration/NoiseGeneration/GenerationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoiseGeneration
+{
+    class GenerationController
+    {
+        public static Queue<NoiseCell> QueueC;
+        const int size = 128;
+        NoiseCell[,] cells = new NoiseCell[size, size];
+
+        public GenerationController() { }
+
+    }
+}
---
+++ NoiseGeneration/NoiseGeneration/NoiseCell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoiseGeneration
+{
+    class NoiseCell
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public double value { get; set; }
+
+
+    }
+}
